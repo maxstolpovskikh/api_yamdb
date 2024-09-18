9c2e205b4fa3a263a66dba91eb86faecb2878325
Evgeny Pozdnyakov <vz174@mail.ru>
2024-09-18 21:19:57 +0500

    Title,genre,category added

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class GetTitleSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели произведений при GET-запросах."""
+
+    genre = GenreSerializer(
+        many=True,
+        read_only=True
+    )
+    category = CategorySerializer(
+        read_only=True
+    )
+    rating = serializers.IntegerField(
+        read_only=True
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+    def to_representation(self, title):
+        serializer = GetTitleSerializer(title)
+        return serializer.data
